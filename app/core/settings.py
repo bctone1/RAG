611304b58c11b29e147b037ce2c01be0c6f8471f
Damien <damien.park.personal@gmail.com>
2025-08-29 14:00:40 +0900

    2025-08-29 : .env-> settings.py 에서 받음 변수설정 용이 해지고 .env dotenv로 관리해서 git 관리 할때 config 에 민감정보+필요정보 가 중첩돼 있는 부분을 분리함

--- app/core/settings.py
+++ app/core/settings.py
@@ -1,10 +1,10 @@
 import os
-from pydantic import AnyUrl
+from pydantic import AnyUrl, Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file=".env",           # .env 허용
+        env_file=".env",           # .env <- 이 경로를 탐색함
         env_file_encoding="utf-8",
         case_sensitive=True,
         extra="ignore",            # 정의되지 않은 키 무시
