63a63c9acbf945257e3305e6a53ebf3724bd8389
Damien <damien.park.personal@gmail.com>
2025-09-03 16:15:05 +0900

    2025-09-03 : env

--- alembic/env.py
+++ alembic/env.py
@@ -2,7 +2,7 @@ from logging.config import fileConfig
 import os
 from sqlalchemy import engine_from_config, pool
 from alembic import context
-from app.db.base import get_database_url
+
 from app.db.models import Base
 from dotenv import load_dotenv
 
